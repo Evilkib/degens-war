8eaf26a05615e064abbda4942dd803a803b84845
Evilkib <97460030+Evilkib@users.noreply.github.com>
2026-03-17 17:25:32 +0845

    Update next.config.js to add headers and image formats

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Your Next.js configuration
+  transpilePackages: ["@coinbase/onchainkit"],
+  async headers() {
+    return [
+      {
+        source: "/.well-known/farcaster.json",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Content-Type", value: "application/json" },
+        ],
+      },
+    ];
+  },
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 module.exports = nextConfig;
